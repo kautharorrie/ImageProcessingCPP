ee48feb10b35f2e46f1822ad1cb5fccd5613a324
kautharorrie <orrkau001@myuct.ac.za>
2023-03-30 16:44:33 +0200

    adding functionality for reading in a pgm file

--- driver.cpp
+++ driver.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
     std::cout << "Hello World!" << std::endl;
 
     ORRKAU001::PGMimageProcessor image;
-    
+    std::cout << image.extractComponents(200, 1) << std::endl;
+
     return 0;
     
    

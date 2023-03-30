2f5f7953fb12a4cf3fc340eb2cb43f9309ff9d1b
root <root@LAPTOP-5C8A7RPD>
2023-03-30 14:56:19 +0200

    created the files for the ConnectedComponent: .h and .cpp

---
+++ ConnectedComponent.cpp
@@ -0,0 +1,59 @@
+#include "ConnectedComponent.h"
+
+//default constructor
+ORRKAU001::ConnectedComponent::ConnectedComponent()
+{
+    ORRKAU001::ConnectedComponent::setID(0);
+}
+
+//parametised constructor
+ORRKAU001::ConnectedComponent::ConnectedComponent(int i)
+{
+    ORRKAU001::ConnectedComponent::setID(i);
+}
+
+//destructor
+ORRKAU001::ConnectedComponent::~ConnectedComponent()
+{
+
+}
+
+/**
+ * set the ID of the connected components 
+ * take in the ID and set the private variable ID 
+ */
+void ORRKAU001::ConnectedComponent::setID(int id)
+{
+    ORRKAU001::ConnectedComponent::ID = id;
+}
+
+/**
+ * set the number of pixels
+ * the number of pixels is equal to the size of the vector
+ */
+void ORRKAU001::ConnectedComponent::setNumberOfPixels(void)
+{
+    //number of pixels is equal to the container size
+    ORRKAU001::ConnectedComponent::numberOfPixels = ORRKAU001::ConnectedComponent::components.size();
+}
+
+/**
+ * return the number of pixels inside the connected component
+ */
+int ORRKAU001::ConnectedComponent::containerSize(void)
+{
+    ORRKAU001::ConnectedComponent::setNumberOfPixels();
+    return ORRKAU001::ConnectedComponent::numberOfPixels;
+}
+
+/*
+ * Add a pixel to the vector to hold the connected components
+*/
+void ORRKAU001::ConnectedComponent::addToVector(std::pair<int, int> p)
+{
+    ORRKAU001::ConnectedComponent::components.push_back(p);
+}
+
+
+
+
---
+++ ConnectedComponent.h
@@ -0,0 +1,63 @@
+/**
+ * .h file 
+ * Assignment 3
+ * CSC3022F 
+ * Author: Kauthar Orrie
+ *
+ */
+ 
+#ifndef CONNECTEDCOMPONENT_H
+#define CONNECTEDCOMPONENT_H
+
+
+#include <string.h>
+#include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <memory>
+#include <queue>
+
+//declaring my namespace with my student number
+namespace ORRKAU001
+{
+    //create Connected Component class
+    class ConnectedComponent
+    {
+        //private variables + functions
+        private: 
+            int numberOfPixels; //number of pixels within the component
+            int ID; //get the ID of the component
+
+            //a container (vector) to store the components
+            std::vector<std::pair<int, int>> components;
+
+        //public variables + functions
+        public:
+            ConnectedComponent(void); //constructor declaration
+            ~ConnectedComponent(); //destructor declaration
+            ConnectedComponent(int ID); //parametised constructor declaration
+
+            //method to set the ID of the connected components
+            void setID(int i);
+
+            //method to get the current size of the container 
+            int containerSize(void);
+
+            //set number of pixels
+            void setNumberOfPixels(void); 
+
+            //add to vector (container)
+            void addToVector(std::pair<int,int> p);
+
+    };
+}
+
+
+#endif

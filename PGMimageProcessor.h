05c3d3c9c4d101f06d3e8ba207b6b853014cd617
kautharorrie <orrkau001@myuct.ac.za>
2023-03-30 15:05:59 +0200

    Skeleton files for the PGMimageProcessor files.

---
+++ PGMimageProcessor.h
@@ -0,0 +1,21 @@
+/**
+ * .h file 
+ * Assignment 3
+ * CSC3022F 
+ * Author: Kauthar Orrie
+ *
+ */
+ 
+#ifndef CONNECTEDCOMPONENT_H
+#define CONNECTEDCOMPONENT_H
+
+#include "ConnectedComponent.h"
+
+//declaring my namespace with my student number
+namespace ORRKAU001
+{
+    //create Connected Component class
+    class ConnectedComponent
+    {};
+}
+#endif
